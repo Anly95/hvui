d43a0da82372100a4a9940dc948212531f3faae7
anly <anly95@foxmail.com>
2022-04-07 23:00:39 +0800

    fix

--- src/layout/components/ToolHeader.vue
+++ src/layout/components/ToolHeader.vue
@@ -2,7 +2,6 @@
 import { defineComponent, computed } from 'vue'
 import { Collapse } from '@/components/Collapse'
 import { LocaleDropdown } from '@/components/LocaleDropdown'
-import { SizeDropdown } from '@/components/SizeDropdown'
 import { UserInfo } from '@/components/UserInfo'
 import { Screenfull } from '@/components/Screenfull'
 import { Breadcrumb } from '@/components/Breadcrumb'
@@ -23,9 +22,6 @@ const hamburger = computed(() => appStore.getHamburger)
 
 // 全屏图标
 const screenfull = computed(() => appStore.getScreenfull)
-
-// 尺寸图标
-const size = computed(() => appStore.getSize)
 
 // 布局
 const layout = computed(() => appStore.getLayout)
@@ -55,9 +51,6 @@ export default defineComponent({
         <div class="h-full flex items-center">
           {screenfull.value ? (
             <Screenfull class="hover-tigger" color="var(--top-header-text-color)"></Screenfull>
-          ) : undefined}
-          {size.value ? (
-            <SizeDropdown class="hover-tigger" color="var(--top-header-text-color)"></SizeDropdown>
           ) : undefined}
           {locale.value ? (
             <LocaleDropdown
